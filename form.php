5ad288248cedd72c820d3b6ec8760c4804246ade
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-02-16 21:41:52 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,10 +1,17 @@
-<html>
-  <head>
-    <title> Information Sent</title>
-  </head>
-  <body>
-	<?php
-	 echo 'Thanks for submitting the form.<br />';
-	?>
- </body>
-</html>
+<?php
+
+    $host="localhost";
+    $user="root";
+    $password="";
+    $con=mysql_connect($host,$user,$password);
+
+    if(!$con) {
+        echo '<h1>Connected to MySQL</h1>';
+        //if connected then Select Database.
+        $db=mysql_select_db("YOUR_DATABASE_NAME",$con);
+        $query=mysql_query("YOUR_MYSQL_QUERY",$db);
+    } else {
+        echo '<h1>MySQL Server is not connected</h1>';
+    }
+
+?>
