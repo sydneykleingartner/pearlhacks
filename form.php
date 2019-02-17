045fa1a0b1b24738e129e5567e856c5e97fc947c
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-02-16 22:15:05 -0500

    Update form.php

--- form.php
+++ form.php
@@ -8,8 +8,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO user (fname, lname, email)
-    VALUES (:firstname, :lastname, :email)";
+    $sql = $conn->prepare("INSERT INTO user (fname, lname, email) VALUES (:firstname, :lastname, :email)");
     $sql->bindParam(":firstname", $_POST['firstname']);
     $sql->bindParam(":lastname", $_POST['lastname']);
     $sql->bindParam(":email", $_POST['email']);
