ff7977a91ca8b891afce68744e609314df2e55cb
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-02-16 22:05:10 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,12 +1,23 @@
 <?php
-    try {
-        $user="root";
-        $password="";
-        $dbh = new PDO('mysql:host=localhost', $user, $password);
-        echo '<h1>Connected to MySQL</h1>';
-    } catch (PDOException $e) {
-        echo '<h1>MySQL Server is not connected</h1>';
-        echo "Error!: " . $e->getMessage() . "<br/>";
-        die();
+$servername = "192.168.64.2";
+$username = "root";
+$password = "password";
+$dbname = "pearlhacks";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO user (fname, lname, email)
+    VALUES ($_POST['firstname'], $_POST['lastname'], $_POST['email'])";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
     }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
 ?>
